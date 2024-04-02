382405aac308942bba22576f909abfb2b6387d6c
ottcs <ottcs20@gmail.com>
2024-04-02 16:35:34 -0400

    Starting Point, part 2

---
+++ jmaker-application/src/main/java/app.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author user
+ */
+public class app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello JMaker!");
+    }
+}
