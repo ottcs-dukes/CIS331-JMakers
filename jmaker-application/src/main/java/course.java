d7fad3228a38d975de6efc848600de2f094c3253
Ryan-Curran <curranra@dukes.jmu.edu>
2024-04-03 17:32:49 -0400

    Course and Semester

---
+++ jmaker-application/src/main/java/course.java
@@ -0,0 +1,47 @@
+public class Course {
+
+    public String prefix;
+    public int number;
+    public String name;
+    public String[] days;
+    public String startTime;
+    public String endTime;
+    public int creditHours;
+    public Student[] enrollment;
+    public Semester semester;
+
+    public Course(String prefix, int number, String name, String[] days, String startTime,
+        String endTime, int creditHours, Semester semester) {
+
+        this.prefix = prefix;
+        this.number = number;
+        this.name = name;
+        this.days = days;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.creditHours = creditHours;
+        this.enrollment = enrollment;
+        this.semester = semester;
+    }
+
+    public void addStudent(Student stu) {
+        for (int i = 0; i < enrollment.length; i++) {
+            if (enrollment[i] == null) {
+                enrollment[i] = stu;
+                return;
+            }
+        }
+        System.out.println("No available seats.");
+    }
+
+    public void removeStudent(Student stu) {
+        for (int i = 0; i < enrollment.length; i++) {
+            if (enrollement[i] == stu) {
+                enrollement[i] = null;
+                return;
+            }
+        }
+        System.out.println("No student found.");
+    }
+
+}
