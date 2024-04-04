069b983dcd41c13e9bbab69d8e2e41276f3a35dd
Ryan-Curran <curranra@dukes.jmu.edu>
2024-04-04 16:36:48 -0400

    Student getNewID

--- jmaker-application/src/main/java/Student.java
+++ jmaker-application/src/main/java/Student.java
@@ -6,50 +6,53 @@ public class Student {
     private String email;
     private double gpa;
     private int ssn; // did not include emergency contact for now
-    public static int[] stuIDs;
-    
-public Student(int studentID, String fullName, String Address, 
-              String email, double gpa, int ssn){
-    this.studentID = studentID;
-    this.fullName = fullName;
-    this.Address = Address;
-    this.email = email;
-    this.gpa = gpa;
-    this.ssn = ssn; // did not include the this.stuIDs
-    
-}    
+    private static int prevStuID = 199;
+        
+    public Student(String fullName, String Address, 
+                String email, double gpa, int ssn){
+        this.studentID = studentID;
+        this.fullName = fullName;
+        this.Address = Address;
+        this.email = email;
+        this.gpa = gpa;
+        this.ssn = ssn; // did not include the this.stuIDs
+        
+    }    
 
-public void editStudent(String fullName, String Address, 
-              String email, double gpa){
-    this.fullName = fullName;
-    this.Address = Address;
-    this.email = email;
-    this.gpa = gpa;
-}
+    public void editStudent(String fullName, String Address, 
+                String email, double gpa){
+        this.fullName = fullName;
+        this.Address = Address;
+        this.email = email;
+        this.gpa = gpa;
+    }
 
-public Double getGPA(){
-    return this.gpa;
-}   
-    
-public int getSSN(){
-    return this.ssn;
-}    
+    public Double getGPA(){
+        return this.gpa;
+    }   
+        
+    public int getSSN(){
+        return this.ssn;
+    }    
 
-public String getAddress(){
-    return this.Address;
-}
+    public String getAddress(){
+        return this.Address;
+    }
 
-public int getStuID(){
-    
-    return this.studentID;
-    
-}
+    public int getStuID(){
+        
+        return this.studentID;
+        
+    }
 
-// Override
-public boolean equals(Student stu) {
-    return this.fullName.equals(stu.fullName);
-}
+    // Override
+    public boolean equals(Student stu) {
+        return this.fullName.equals(stu.fullName);
+    }
 
-
+    private static int getNewID() {
+        int newID = prevStuID++;
+        return newID;
+    }
 
 }
