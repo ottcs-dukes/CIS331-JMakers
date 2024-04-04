77c38cd1d6ce8e052918d586798835016d48575c
fannoner <erfannon26@gmail.com>
2024-04-04 16:42:11 -0400

    Update Faculty and Edit Student

--- jmaker-application/src/main/java/Faculty.java
+++ jmaker-application/src/main/java/Faculty.java
@@ -1,14 +1,14 @@
 public class Faculty {
     
     private int facultyID;
-    public String name;
+    private String name;
     private String email;
-    public String buildingName;
-    public int officeNumber;
+    private String buildingName;
+    private int officeNumber;
     private int phoneNumber;
-    public String department;
-    public String position;
-    private static int[] facIDs = new int[50];
+    private String department;
+    private String position;
+    private static int prevFacID = 99;
     
     public Faculty (String name, String email, String buildingName, 
                         int officeNumber, int phoneNumber,
@@ -21,6 +21,16 @@ public class Faculty {
         this.officeNumber = officeNumber;
         this.phoneNumber = phoneNumber;
         this.department = department;
+        this.position = position;
+    
+    }
+    
+    public void editFaculty(String email, 
+                        int officeNumber, int phoneNumber, String position) {
+        
+        this.email = email;
+        this.officeNumber = officeNumber;
+        this.phoneNumber = phoneNumber;
         this.position = position;
     
     }
@@ -38,18 +48,12 @@ public class Faculty {
     }
 
     private static int getNewId() {
-        if (facIDs[0] == null) {
-            int id = 100;
-            facIDs[0] = id;
-            return id;
-        } else {
-            for (int i = 1; i < facIDs.length; i++) {
-                if (facIDs[i] == null) {
-                    facIDs[i] = facIDs[i - 1]++;
-                    return facIDs[i];
-                }
-            }
+        int newID = prevFacID++;
+        return newID;
+     
         }
-    }
+    
 
 }
+
+
