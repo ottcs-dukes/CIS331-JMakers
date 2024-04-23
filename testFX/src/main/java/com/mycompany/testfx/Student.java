02816530ec6b36690b1f3bb321f8f1fe1f3974de
Ryan-Curran <curranra@dukes.jmu.edu>
2024-04-23 19:20:00 -0400

    Student.create()

--- testFX/src/main/java/com/mycompany/testfx/Student.java
+++ testFX/src/main/java/com/mycompany/testfx/Student.java
@@ -1,5 +1,12 @@
 package com.mycompany.testfx;
 import java.util.ArrayList;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class Student {
     
@@ -14,8 +21,52 @@ public class Student {
     
     static void create(ArrayList<Student> students) {
     	// Create a window
+        Stage stage = new Stage();
+        
+        Label lblName = new Label("Full Name:");
+        TextField txtName = new TextField();
+        Label lblAddress = new Label("Address:");
+        TextField txtAddress = new TextField();
+        Label lblEmail = new Label("Email:");
+        TextField txtEmail = new TextField();
+        Label lblGpa = new Label("GPA:");
+        TextField txtGpa = new TextField();
+        Label lblSsn = new Label("SSN:");
+        TextField txtSsn = new TextField();
+        Button btnAdd = new Button("Add Student");
+                
+        GridPane grid = new GridPane();
+        grid.add(lblName, 0, 0);
+        grid.add(txtName, 1, 0);
+        grid.add(lblAddress, 0, 1);
+        grid.add(txtAddress, 1, 1);
+        grid.add(lblEmail, 0, 2);
+        grid.add(txtEmail, 1, 2);
+        grid.add(lblGpa, 0, 3);
+        grid.add(txtGpa, 1, 3);
+        grid.add(lblSsn, 0, 4);
+        grid.add(txtSsn, 1, 4);
+        grid.add(btnAdd, 0, 5);
+        
+        grid.setAlignment(Pos.CENTER);
+        
     	// On button click, append new Student() to students
-    	System.out.println("Create Student");
+        btnAdd.setOnAction(e -> {
+            String name = txtName.getText();
+            String address = txtAddress.getText();
+            String email = txtEmail.getText();
+            double gpa = Double.valueOf(txtGpa.getText());
+            int ssn = Integer.valueOf(txtSsn.getText());
+            
+            Student stu = new Student(name, address, email, gpa, ssn);
+            students.add(stu);
+            stage.close();
+        });
+        
+        Scene scene = new Scene(grid, 300, 200);
+        stage.setScene(scene);
+        stage.show();
+        
     }
     
     static void edit(Student target) {
