4a3d31bdfdd561ecf87920b12633cd43f7e2d9b5
fannoner <erfannon26@gmail.com>
2024-04-25 16:49:13 -0400

    meh

--- testFX/src/main/java/com/mycompany/testfx/Faculty.java
+++ testFX/src/main/java/com/mycompany/testfx/Faculty.java
@@ -91,7 +91,67 @@ public class Faculty {
     static void edit(Faculty faculty) {
     	// Create a window with data fields filled
     	// On button click, iterate through each field and set each
-    	System.out.println("Edit Faculty");
+    	Stage stage = new Stage();
+        
+        stage.setTitle("Edit Faculty");
+
+        
+        Label lblName = new Label("Full Name:");
+            TextField txtName = new TextField(faculty.getName());
+        Label lblEmail = new Label("Email:");
+            TextField txtEmail = new TextField(faculty.getEmail());
+        Label lblBuilding = new Label("Building Name:");
+            TextField txtBuilding = new TextField(faculty.getBuilding());
+        Label lblOffice = new Label("Office Number:");
+            TextField txtOffice = new TextField(faculty.getOffice() + "");
+        Label lblPhone = new Label("Phone Number:");
+            TextField txtPhone = new TextField(faculty.getPhone() + "");
+        Label lblDepartment = new Label("Department:");
+            TextField txtDepartment = new TextField(faculty.getDepartment());
+        Label lblPosition = new Label("Position:");
+            TextField txtPosition = new TextField(faculty.getPosition());
+        Button btnAdd = new Button("Edit Faculty");
+            
+        GridPane grid = new GridPane();
+        
+        grid.add(lblName , 0, 0);
+        grid.add(txtName, 1, 0);
+        grid.add(lblEmail, 0, 1);
+        grid.add(txtEmail, 1, 1);
+        grid.add(lblBuilding, 0, 2);
+        grid.add(txtBuilding, 1, 2);
+        grid.add(lblOffice, 0, 3);
+        grid.add(txtOffice, 1, 3);
+        grid.add(lblPhone, 0, 4);
+        grid.add(txtPhone, 1, 4);
+        grid.add(lblDepartment, 0, 5);
+        grid.add(txtDepartment, 1, 5);
+        grid.add(lblPosition, 0, 6);
+        grid.add(txtPosition, 1, 6);
+        grid.add(btnAdd, 0, 7);
+        
+        grid.setAlignment(Pos.CENTER);
+        
+        btnAdd.setOnAction(e -> {
+            String name = txtName.getText();
+            String email = txtEmail.getText();
+            String building = txtBuilding.getText();
+            int office = Integer.valueOf(txtOffice.getText());
+            int phone = Integer.valueOf(txtPhone.getText());
+            String department = txtDepartment.getText();
+            String position = txtPosition.getText();
+            
+            
+            faculty.editFaculty(name, email, building, office, phone, department, position);
+            stage.close();
+            
+            
+        });
+        
+        Scene scene = new Scene(grid, 300, 300);
+        stage.setScene(scene);
+        stage.show();
+            
     }
     
     public Faculty (String name, String email, String buildingName, 
@@ -131,6 +191,18 @@ public class Faculty {
     }
     public String getEmail(){
         return this.email;
+    }
+    public String getBuilding(){
+        return this.buildingName;
+    }
+    public int getOffice(){
+        return this.officeNumber;
+    }
+    public String getDepartment(){
+        return this.department;
+    }
+    public String getPosition(){
+        return this.position;
     }
 
     public long getPhone(){
